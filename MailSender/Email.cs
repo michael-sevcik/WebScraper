1b2284bf7689c9edf9eddfd7de08cb7851157dbd
Michael Ševčík <69696161@cuni.cz>
2023-07-27 22:29:56 +0200

    Add email sender

---
+++ MailSender/Email.cs
@@ -0,0 +1,17 @@
+﻿namespace MailSender;
+
+/// <summary>
+/// Struct <c>Email</c> encapsulates its subject and message.
+/// </summary>
+public readonly struct Email
+{
+    /// <summary>
+    /// The subject of the email.
+    /// </summary>
+    public readonly string subject;
+
+    /// <summary>
+    /// The content of an email that is displayed when addressee opens it. 
+    /// </summary>
+    public readonly string body;
+}
---
+++ MailSender/IMailSender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender
+{
+    /// <summary>
+    /// Utility class to send emails.
+    /// </summary>
+    public interface IMailSender
+    {
+        /// <summary>
+        /// Asynchronously sends the <paramref name="email"/>.
+        /// </summary>
+        /// <param name="email">The email to be send.</param>
+        /// <returns>Email sending task.</returns>
+        Task SendEmailAsync(Email email);
+    }
+}
