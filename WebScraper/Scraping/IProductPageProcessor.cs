81fb228df67e9d7f29d34479cfa700a727429d62
Michael Ševčík <69696161@cuni.cz>
2023-08-05 10:21:40 +0200

    Add interface for product page processors

---
+++ WebScraper/Scraping/IProductPageProcessor.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+using ProductListCrawler;
+using WebScraper.AuctionRecord;
+
+namespace WebScraper.Scraping;
+
+/// <summary>
+/// Class for processing product pages.
+/// </summary>
+public interface IProductPageProcessor
+{
+    /// <summary>
+    /// Parses the product pages and extracts all the information into a <see cref="BaseAuctionRecord"/> as an asynchronous operation.
+    /// </summary>
+    /// <param name="htmlDocument">The product page to be parsed.</param>
+    /// <returns>
+    /// The task object representing the asynchronous operation.
+    /// The Result property on the task object returns parsed information in a instance of <see cref="BaseAuctionRecord"/>.
+    /// </returns>
+    /// <exception cref="ParseException">This exception is thrown when an error occurred during parsing of <paramref name="htmlDocument"/>.</exception>
+    Task<BaseAuctionRecord> ParseProductPage(HtmlDocument htmlDocument);
+}
