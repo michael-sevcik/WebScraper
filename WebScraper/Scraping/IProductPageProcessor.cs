d11909b63db3f46a287029a3a22ee19e135706db
Michael Ševčík <69696161@cuni.cz>
2023-08-05 16:20:26 +0200

    Fix a typo

--- WebScraper/Scraping/IProductPageProcessor.cs
+++ WebScraper/Scraping/IProductPageProcessor.cs
@@ -15,8 +15,8 @@ public interface IProductPageProcessor
     /// <param name="htmlDocument">The product page to be parsed.</param>
     /// <returns>
     /// The task object representing the asynchronous operation.
-    /// The Result property on the task object returns parsed information in a instance of <see cref="BaseAuctionRecord"/>.
+    /// The Result property on the task object returns parsed information in an instance of <see cref="BaseAuctionRecord"/>.
     /// </returns>
     /// <exception cref="ParseException">This exception is thrown when an error occurred during parsing of <paramref name="htmlDocument"/>.</exception>
-    Task<BaseAuctionRecord> ParseProductPage(HtmlDocument htmlDocument);
+    Task<BaseAuctionRecord> ParseProductPageAsync(HtmlDocument htmlDocument);
 }
