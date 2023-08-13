e4389bbd5fde28a3d02a8fd12b5d3e39a42b2421
Michael Ševčík <69696161@cuni.cz>
2023-08-13 16:15:11 +0200

    Add a notifier

---
+++ WebScraper/Notifications/INotifier.cs
@@ -0,0 +1,14 @@
+﻿namespace WebScraper.Notifications;
+
+/// <summary>
+/// Represents an objects that sends notifications.
+/// </summary>
+internal interface INotifier
+{
+    /// <summary>
+    /// Asynchronously sends the <paramref name="notification"/>.
+    /// </summary>
+    /// <param name="notification">The description of the new event.</param>
+    /// <returns>Task object representing the asynchronous operation.</returns>
+    Task NotifyAsync(Notification notification);
+}
---
+++ WebScraper/Notifications/Notification.cs
@@ -0,0 +1,22 @@
+﻿namespace WebScraper.Notifications;
+
+/// <summary>
+/// Represents a notification of an event.
+/// </summary>
+internal readonly struct Notification
+{
+    /// <summary>
+    /// What kind of an event occurred.
+    /// </summary>
+    public readonly string Purpose;
+
+    /// <summary>
+    /// Short description of the event.
+    /// </summary>
+    public readonly string Title;
+
+    /// <summary>
+    /// The description of the event.
+    /// </summary>
+    public readonly string Message;
+}
