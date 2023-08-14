58bfdeac624687e9aba1f0ca94865dc0e73f6550
Michael Ševčík <69696161@cuni.cz>
2023-08-14 06:49:35 +0200

    Add auction record manager interface

---
+++ WebScraper/AuctionRecord/IAuctionRecordManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WebScraper.AuctionRecord;
+
+/// <summary>
+/// Represents a class for managing auction records.
+/// </summary>
+internal interface IAuctionRecordManager
+{
+    /// <summary>
+    /// Handles the <paramref name="record"/> - stores it if it is new, or sends a notification if it was readded.
+    /// </summary>
+    /// <param name="record">The record of a auction to be handled.</param>
+    /// <returns>The task object representing the asynchronous handling.</returns>
+    Task HandleRecordAsync(BaseAuctionRecord record);
+
+    /// <summary>
+    /// Updates the auction record with the <paramref name="id"/>.
+    /// </summary>
+    /// <param name="id">Id of the ending auction record.</param>
+    /// <param name="record">The current data of the auction.</param>
+    /// <returns>The task object representing the asynchronous updating.</returns>
+    Task UpdateEndingAuctionAsync(Guid id, BaseAuctionRecord record);
+}
