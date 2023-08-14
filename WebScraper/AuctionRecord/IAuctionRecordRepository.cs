0ac95ed46adc98d36cc2307d0f414d5e57137409
Michael Ševčík <69696161@cuni.cz>
2023-08-14 06:48:29 +0200

    Make the repository non-generic

---
+++ WebScraper/AuctionRecord/IAuctionRecordRepository.cs
@@ -0,0 +1,72 @@
+﻿namespace WebScraper.AuctionRecord
+{
+    /// <summary>
+    /// Represents generic auction record repository that enables storing and managing of <see cref="BaseAuctionRecord"/> entities.
+    /// </summary>
+    public interface IAuctionRecordRepository
+    {
+        /// <summary>
+        /// tries to asynchronously get the <see cref="BaseAuctionRecord"/> entity with the specified <paramref name="uniqueIdentifier"/>.
+        /// </summary>
+        /// <param name="uniqueIdentifier">
+        /// <see cref="BaseAuctionRecord.UniqueIdentifier"/> of the desired <see cref="BaseAuctionRecord"/> entity.
+        /// </param>
+        /// <param name="record">A record with the <paramref name="uniqueIdentifier"/>.</param>
+        /// <returns>
+        /// The task object representing the asynchronous operation. The Result property on the task object
+        /// returns true if a record with the <paramref name="uniqueIdentifier"/> exists, otherwise false.
+        /// </returns>
+        Task<bool> TryGetAsync(string uniqueIdentifier, out BaseAuctionRecord record);
+
+        /// <summary>
+        /// Gets asynchronously the <see cref="BaseAuctionRecord"/> entity with the specified <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id"><see cref="BaseAuctionRecord.Id"/> of the desired <see cref="BaseAuctionRecord"/> entity.</param>
+        /// <returns>
+        /// The task object representing the asynchronous operation. The Result property on the task object returns
+        /// the <see cref="BaseAuctionRecord"/> entity with the <paramref name="id"/>.
+        /// </returns>
+        /// <exception cref="InvalidOperationException"> Is thrown in case that an entity with the specified <paramref name="id"/> is not found.</exception>
+        Task<BaseAuctionRecord> GetAsync(Guid id);
+
+        /// <summary>
+        /// Gets asynchronously all the <see cref="BaseAuctionRecord"/> entities.
+        /// </summary>
+        /// <returns>
+        /// The task object representing the asynchronous operation. The Result property on the task object
+        /// returns <see cref="IEnumerable{T}"/> containing all records.
+        /// </returns>
+        Task<IEnumerable<BaseAuctionRecord>> GetAllAsync();
+
+        /// <summary>
+        /// Gets asynchronously all the <see cref="BaseAuctionRecord"/> entities.
+        /// </summary>
+        /// <param name="dateTime">The date used for filtering.</param> // TODO:
+        /// <returns>
+        /// The task object representing the asynchronous operation. The Result property on the task object
+        /// returns <see cref="IEnumerable{T}"/> containing all records.
+        /// </returns>
+        Task<IEnumerable<BaseAuctionRecord>> GetAllFromDateAsync(DateTime dateTime);
+
+        /// <summary>
+        /// Asynchronously stores the new <paramref name="record"/>. // TODO: decide what should happen in case that the entity is already stored.
+        /// </summary>
+        /// <param name="record">The record to add.</param>
+        /// <returns>The task object representing the asynchronous storing.</returns>
+        Task AddAsync(BaseAuctionRecord record);
+
+        /// <summary>
+        /// Deletes the specified <see cref="BaseAuctionRecord"/> entity as an asynchronous operation using a <see cref="Task"/> object.
+        /// </summary>
+        /// <param name="id"><see cref="BaseAuctionRecord.Id"/> of the desired <see cref="BaseAuctionRecord"/> entity.</param>
+        /// <returns>The task object representing the asynchronous deletion.</returns>
+        Task DeleteAsync(Guid id);
+
+        /// <summary>
+        /// Updates the stored data of the <see cref="BaseAuctionRecord"/> entity with matching <see cref="BaseAuctionRecord.Id"/> as an asynchronous operation using a <see cref="Task"/> object.
+        /// </summary>
+        /// <param name="record">The entity to be updated.</param>
+        /// <returns>The task object representing the asynchronous updating.</returns>
+        Task UpdateAsync(BaseAuctionRecord record);
+    }
+}
