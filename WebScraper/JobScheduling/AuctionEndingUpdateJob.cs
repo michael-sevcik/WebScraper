a6c5ea75763440f28cdb0443aedb87b5665e9556
Michael Ševčík <69696161@cuni.cz>
2023-08-14 21:27:02 +0200

    Add auction ending update job

---
+++ WebScraper/JobScheduling/AuctionEndingUpdateJob.cs
@@ -0,0 +1,25 @@
+﻿using Quartz;
+using WebScraper.Scraping;
+
+namespace WebScraper.Jobs;
+
+/// <summary>
+/// Job that updates a record of an auction when the auction is about to end.
+/// </summary>
+/// <remarks>
+/// The <see cref="IJobExecutionContext.MergedJobDataMap"/> is expected to contain record's <see cref="Guid"/> under "id" key,
+/// <see cref="Uri"/> link to the product page under "link"
+/// and appropriate <see cref="IProductPageLinkHandler"/> under "productPageLinkHandler".
+/// </remarks>
+internal sealed class AuctionEndingUpdateJob : IJob
+{
+    /// <inheritdoc/>
+    public async Task Execute(IJobExecutionContext context)
+    {
+        var link = (Uri)context.MergedJobDataMap["link"];
+        var id = context.MergedJobDataMap.GetGuid("id");
+        var productPagelinkHandler = (IProductPageLinkHandler)context.MergedJobDataMap["productPageLinkHandler"];
+
+        await productPagelinkHandler.UpdateAuctionRecordAsync(link, id);
+    }
+}
