a2f6a9b26cb096f95a27d95ac17f9f5f655c7093
Michael Ševčík <69696161@cuni.cz>
2023-09-15 07:49:32 +0200

    Remove usings

--- WebScraperTests/MockHtmlDownloader.cs
+++ WebScraperTests/MockHtmlDownloader.cs
@@ -1,10 +1,5 @@
 ﻿using Downloader;
 using HtmlAgilityPack;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebScraperTests
 {
