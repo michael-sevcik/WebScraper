4d001513a5b34ba6aef01bd4cd87a10d06d61743
Michael Ševčík <69696161@cuni.cz>
2023-09-16 21:55:53 +0200

    Add documentation

--- WebScraper/Persistence/AuctionRecord/AdditionalInfromationPair.cs
+++ WebScraper/Persistence/AuctionRecord/AdditionalInfromationPair.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace WebScraper.Persistence.AuctionRecord;
 
+/// <summary>
+/// Encapsulation of an information and its name.
+/// </summary>
 public class AdditionalInfromationPair
 {
+    /// <summary>
+    /// Gets the name of <see cref="Value"/>.
+    /// </summary>
     public string Name { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Gets the value of <see cref="Name"/>.
+    /// </summary>
     public string Value { get; init; } = string.Empty;
 }
