b8231e116daebbde330c7c3dc50d330eb203d783
Michael Ševčík <69696161@cuni.cz>
2023-09-16 21:54:52 +0200

    Add interface for job scheduler

---
+++ WebScraper/JobScheduling/IJobScheduler.cs
@@ -0,0 +1,19 @@
+﻿using WebScraper.Scraping;
+
+namespace WebScraper.JobScheduling;
+
+/// <summary>
+/// Represents a class that schedules the update jobs.
+/// </summary>
+public interface IJobScheduler
+{
+    /// <summary>
+    /// Schedules the job with the specified data.
+    /// </summary>
+    /// <param name="jobStart">When should the job start.</param>
+    /// <param name="link">The source from which the job should be downloaded.</param>
+    /// <param name="id">The id of the record.</param>
+    /// <param name="pageProcessor">Processor that should parse the downloaded page.</param>
+    /// <returns>A task object that represents the asynchronous operation.</returns>
+    Task ScheduleUpdateJobAsync(DateTime jobStart, Uri link, Guid id, IProductPageProcessor pageProcessor);
+}
--- WebScraper/JobScheduling/JobScheduler.cs
+++ WebScraper/JobScheduling/JobScheduler.cs
@@ -8,7 +8,7 @@ namespace WebScraper.JobScheduling;
 /// <summary>
 /// Scheduler for scheduling update jobs. <see cref="AuctionEndingUpdateJob"/>.
 /// </summary>
-internal sealed class JobScheduler
+internal sealed class JobScheduler : IJobScheduler
 {
     private static readonly TimeSpan TimeReserve = TimeSpan.FromSeconds(70);
     private readonly ILogger<JobScheduler> logger;
@@ -32,14 +32,7 @@ internal sealed class JobScheduler
         this.scheduler = schedulerTask.Result;
     }
 
-    /// <summary>
-    /// Schedules the job with the specified data.
-    /// </summary>
-    /// <param name="jobStart">When should the job start.</param>
-    /// <param name="link">The source from which the job should be downloaded.</param>
-    /// <param name="id">The id of the record.</param>
-    /// <param name="pageProcessor">Processor that should parse the downloaded page.</param>
-    /// <returns>A task object that represents the asynchronous operation.</returns>
+    /// <inheritdoc/>
     public async Task ScheduleUpdateJobAsync(
         DateTime jobStart,
         Uri link,
@@ -63,7 +56,7 @@ internal sealed class JobScheduler
             .ForJob(AuctionEndingUpdateJob.Key)
             .Build();
 
-        this.logger.LogInformation($"Scheduling an update job for the product on page {link}. The job start: {startTime}");
+        this.logger.LogInformation("Scheduling an update job for the product on page {link}. The job start: {startTime}", link, startTime);
 
         // Schedule the job
         await this.scheduler.ScheduleJob(trigger);
